bc07397b48035b3737f62d38fc6ebea6e8669ec5
susheel esam <esamuday1995@gmail.com>
2026-09-10 23:10:02 +0530

    files2

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
-const heading = React.createElement("h1", {id:heading}, "Hello");
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "heading" }, "Hello I'm Heading")
+  )
+);
+
+//const heading = React.createElement("h1",{id:"heading"}, "Hello Uday!");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
